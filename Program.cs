f73cd072f4ab082e32d310827131cd2ef1b1c5f2
onny <onny.s@students.amikom.ac.id>
2020-04-10 16:09:08 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InheritanceLatihan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
