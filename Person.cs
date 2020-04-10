6ae1f7e57bf60329511643a045d7c9761c4bf553
onny <onny.s@students.amikom.ac.id>
2020-04-10 16:20:42 +0700

    penambahan class baru

---
+++ Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void GetNameAndAge { } 
+    {
+        Console.WriteLine("Rge Person Has name : {0} and age : {1}", Name, Age);    
+    }
+}
