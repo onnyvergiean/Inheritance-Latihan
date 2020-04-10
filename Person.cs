4dea946795df37b372aac85e60ff5652df067181
onny <onny.s@students.amikom.ac.id>
2020-04-10 16:36:38 +0700

    Finishing Program

--- Person.cs
+++ Person.cs
@@ -5,8 +5,9 @@ public class Person
     public string Name { get; set; }
     public int Age { get; set; }
 
-    public void GetNameAndAge { } 
+    public void GetNameAndAge()
     {
-        Console.WriteLine("The Person Has name : {0} and age : {1}", Name, Age);    
-    }
+        Console.WriteLine("The Person Has name : {0} and Age : {1}", Name, Age);
+    } 
+    
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace InheritanceLatihan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person = new Person();
+            person.Name = "Thomas";
+            person.Age = 20;
+            person.GetNameAndAge();
+
+
+            Teacher teacher = new Teacher();
+            teacher.Name = "Thomas";
+            teacher.Age = 20;
+            teacher.TeacherId = "190302";
+            teacher.Subject = "Math";
+            teacher.GetNameAndAge();
+
+            Student student = new Student();
+            student.Name = "Thomas";
+            student.Age = 20;
+            student.StudentId = "190302";
+            student.Email = "onny.s@students.amikom.ac.id";
+            student.GetNameAndAge();
+
+            Console.ReadKey();
         }
     }
-}
+ }
+
